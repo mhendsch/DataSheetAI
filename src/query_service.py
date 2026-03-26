df30adfe252df3a3a436510f7e2e5e00a0d2c567
mhendsch <mhendsch@bu.edu>
2026-03-26 18:07:14 -0400

    Create query_service stub

---
+++ src/query_service.py
@@ -0,0 +1,15 @@
+import sqlite3
+import pandas
+import schema_manager
+
+
+print("What would you like to do today?: ")
+user_input = input()
+while (user_input.lower() != "q"):
+    print("What would you like to do today?: ")
+    user_input = input()
+
+    # Call a function depending on input
+    # Validate against Database
+
+    # If valid, call LLM Adapter
