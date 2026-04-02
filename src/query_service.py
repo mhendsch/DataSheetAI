8babd55e3e1aec3d45142ac18fb395a9765abb7b
mhendsch <mhendsch@bu.edu>
2026-04-02 14:18:58 -0400

    Add data loading logic to query_service

--- src/query_service.py
+++ src/query_service.py
@@ -3,6 +3,25 @@ import pandas
 import schema_manager
 import sql_validator
 
+DB = "my_database.db"
+
+def loadData(filename, table_name):
+    df = csv_loader.loadCSV(filename)
+
+
+    csv_schema = schema_manager.getDataframeSchema(df)
+    db_schema = schema_manager.getTableSchema(DB)
+    # Compare schemas, see if any match, if so append instead of creating new table
+    for table, columns in db_schema.items():
+        if columns == csv_schema:
+            print(f"Schema of CSV file matches existing table '{table}'. Appending data to this table.")
+            csv_loader.insertData(DB, df, table)
+            return 0
+    # If no matching schema, create new table and insert data
+    print(f"No existing table matches the schema of the CSV file. Creating new table '{table_name}' and inserting data.")
+    csv_loader.createTable(DB, df.columns.tolist(), table_name)
+    csv_loader.insertData(DB, df, table_name)
+    return 0
 
 
 print("What would you like to do today?: ")
